307babb6dba92fe5e09d9a8947e399388a19d7aa
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2019-05-11 20:46:22 +0200

    Update run.pyx

--- run.pyx
+++ run.pyx
@@ -1,15 +1,2 @@
-import os
-import sys
-from pathlib import Path
-
-
-
-
-mypath = Path().absolute()
-
-
-
-print(mypath)
-print("Share a kiss with your sys:\n"+sys.copyright)
-print(os.path.join(mypath, ""))
+print("please kill me")
 
