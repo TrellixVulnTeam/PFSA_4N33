5d8db83373e899a20f40f8a7bb04cf046f6445f9
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2019-05-12 15:42:33 +0200

    Command Line Utility

--- run.py
+++ run.py
@@ -1,37 +1,11 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.core.image import Image
-import kivy
-from kivy.animation import Animation
-from kivy.app import App
-from kivy.uix.button import Button
 import os
+import sys
 
-
-
-
-class TestApp(App):
-
-    def animate(self, instance):
-        # create an animation object. This object could be stored
-        # and reused each call or reused across different widgets.
-        # += is a sequential step, while &= is in parallel
-        animation = Animation(pos=(0, 550), t='linear')
-        animation += Animation(pos=(0, 550), t='linear')
-        animation &= Animation(size=(50, 50))
-        animation += Animation(size=(50, 50))
-
-        # apply the animation on the button, passed in the "instance" argument
-        # Notice that default 'click' animation (changing the button
-        # color while the mouse is down) is unchanged.
-        animation.start(instance)
-
-    def build(self):
-        # create a button, and  attach animate() method as a on_press handler
-        button = Button(size_hint=(0.1, 0.1), text="file",
-                        on_press=self.animate)
-        return button
-
-
-if __name__ == '__main__':
-    TestApp().run()
+for arg in sys.argv:
+    print(arg)
+print("      ")
+print("      ")
+print("      ")
+print("      ")
+for x in range(len(os.listdir())):
+    print(os.listdir()[x])
