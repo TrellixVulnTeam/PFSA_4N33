c41791fbfd1be697b191cf40f4273a1b8a4efd1c
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2019-05-13 12:21:08 +0200

    Added Icon

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ build_exe_options = {"packages": ["os","pathlib","sys"], "excludes": ["tkinter"]
 base = None
 if sys.platform == "win32":
     base = "Console"
-
+    
 setup(  name = "PFSA",
         version = "0.1",
         description = "Python File System Analyser",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("run.py", base=base)])
+        executables = [Executable("run.py", base=base, icon="Assets\icon.ico")])
