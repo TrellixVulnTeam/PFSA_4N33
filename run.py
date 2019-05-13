b9384359fe716e6a83f70eda116c784d169cc00c
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2019-05-13 11:58:07 +0200

    Added open function

--- run.py
+++ run.py
@@ -1,18 +1,56 @@
+#Imports
 import os
 import sys
 import pathlib
+#Easy to use clear command
+clear = lambda: os.system('cls')
 
 def pathlistdisplay(directory):
+    #Cool tree listing...
     print("__________"+str(pathlib.Path(directory))+"__________")
     for x in range(len(os.listdir(directory))):
         print("|-"+os.listdir(directory)[x])
+
+def open(directory):
+    os.system("start "+str(pathlib.Path(directory)))
+
+
+
+
 current_dir = str(pathlib.Path(os.getcwd()))
+#Testing for first argument...
 try:
     a = sys.argv[1]+" "
     pth = True
 except:
     pth = False
+cancellisting = False
 if pth == True:
-    pathlistdisplay(sys.argv[1])
+    if sys.argv[1] == "open":
+        cancellisting = True
+        #Testing if the directory is precised... If not use current working directory
+        try:
+            a = sys.argv[2]+" "
+            directory_precised = True
+        except:
+            directory_precised = False
+        print("Opening...")
+        if directory_precised == True:
+            open(sys.argv[2])
+        if directory_precised == False:
+            open(current_dir)
+    try:
+        if cancellisting == True:
+            pass
+        else:
+            pathlistdisplay(sys.argv[1])
+    except:
+        print("Invalid Directory")
+    
+    else:
+        pass
 else:
-    pathlistdisplay(current_dir)
+    try:
+        pathlistdisplay(current_dir)
+    except:
+        print("Invalid Directory")
