3b221df6954bc1dbc67848f1f209e81b9039f75e
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2019-05-26 12:22:33 +0200

    fsffs

--- setup.bat
+++ setup.bat
@@ -1,5 +1,40 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+
+
 Python36\Scripts\pip uninstall --yes cx_freeze
 Python36\Scripts\pip install cx_freeze
+Python36\Scripts\pip uninstall --yes pillow > nul
+Python36\Scripts\pip install pillow > nul
+Python36\Scripts\pip install pillow > nul
 Python36\python setup.py build
 echo off
 echo " "
