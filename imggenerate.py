7118649cea312f25c18a3c0df6da1947f8deee1b
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2019-05-26 12:29:13 +0200

    oooo

--- imggenerate.py
+++ imggenerate.py
@@ -2,32 +2,11 @@ from PIL import Image, ImageFilter, ImageDraw, ImageFont
 import pathlib
 import os
 import sys
-def paste(destination,sourceimage, xy):
-    sourcepixels = sourceimage.convert("RGBA").load()
-    destinationpixels = destination.convert("RGBA").load()
-    for i in range(sourceimage.size[0]):
-        for j in range(sourceimage.size[1]):
-            destinationpixels[i+xy[0],j+xy[1]] = sourcepixels[i,j]
-    return destination
-def removewhite(sourceimage):
-    sourceimage = sourceimage.convert("RGBA")
-    sourcepixels = sourceimage.load()
-    for i in range(sourceimage.size[0]):
-        for j in range(sourceimage.size[1]):
-            if sourcepixels[i,j] == (255,255,255,255):
-                sourcepixels[i,j] = ((255, 255, 255, 0))
-            else:
-                pass
-    sourceimage.show()
-    return sourceimage
 def generate(path, textsize):
     path = pathlib.Path(path)
     fnt = ImageFont.truetype('C:\\tmp\\PFSA\\font.ttf', 100)
     fntb = ImageFont.truetype('C:\\tmp\\PFSA\\font.ttf', textsize)
     background = Image.open("C:\\tmp\\PFSA\\background.jpg").convert("RGBA")
-    fileicon = Image.open("C:\\tmp\\PFSA\\fileicon.png")
-    fileicon.thumbnail((50,50), Image.ANTIALIAS)
-    fileicon = removewhite(fileicon)
     paddingx = 300
     paddingy = 50
     lineseparation = textsize*2.5
@@ -46,7 +25,6 @@ def generate(path, textsize):
     d.text((paddingy*2,125),str(path), font=fnt, fill=(255,255,255))
     for x in range(len(os.listdir(str(path)))):
         d.line((0,paddingx+((x)*lineseparation),background.size[0],paddingx+((x)*lineseparation)), fill=(255,255,255))
-        background = paste(background,fileicon,(paddingy ,round(paddingx+((x)*lineseparation))))
         d.text((paddingy ,paddingx+((x)*lineseparation)), os.listdir(str(path))[x], font=fntb, fill=(255,255,255))
     return background
 
